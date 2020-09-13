29b0387d66ba59583b9c3da1328f6cf63d91ab7a
Sangho.Nam <namdori61@gmail.com>
2020-09-13 11:59:02 +0900

    Train: Feat trainer.

---
+++ train.py
@@ -0,0 +1,139 @@
+import json
+
+from absl import app, flags, logging
+import torch
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, LearningRateLogger
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from preprocess import KbAlbertCharTokenizer
+from models import KbAlbertClassificationModel
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('input_path', default=None,
+                    help='Path to the training dataset')
+flags.DEFINE_string('label_type', default=None,
+                    help='Label type to train')
+flags.DEFINE_string('tokenizer_config_path', default=None,
+                    help='Pretrained tokenizer config path')
+flags.DEFINE_string('vocab_path', default=None,
+                    help='Pretrained tokenizer vocab path')
+flags.DEFINE_string('model_path', default=None,
+                    help='Pretrained model path')
+flags.DEFINE_string('model_config_path', default=None,
+                    help='Pretrained model config path')
+flags.DEFINE_string('save_dir', default=None,
+                    help='Path to save model')
+flags.DEFINE_string('version', default=None,
+                    help='Explain experiment version')
+flags.DEFINE_integer('cuda_device', default=0,
+                     help='If given, uses this CUDA device in training')
+flags.DEFINE_integer('max_epochs', default=10,
+                     help='If given, uses this max epochs in training')
+flags.DEFINE_integer('batch_size', default=4,
+                     help='If given, uses this batch size in training')
+flags.DEFINE_integer('num_workers', default=0,
+                     help='If given, uses this number of workers in data loading')
+flags.DEFINE_float('lr', default=2e-5,
+                   help='If given, uses this learning rate in training')
+flags.DEFINE_float('weight_decay', default=0.1,
+                   help='If given, uses this weight decay in training')
+flags.DEFINE_integer('warm_up', default=500,
+                     help='If given, uses this warm up in training')
+
+
+def main(argv):
+    f = open(FLAGS.tokenizer_config_path, encoding='UTF-8')
+    tokenizer_config = json.loads(f.read())
+    tokenizer = KbAlbertCharTokenizer(vocab_file=FLAGS.vocab_path,
+                                      pretrained_init_configuration=tokenizer_config)
+    if FLAGS.label_type == 'major':
+        model = KbAlbertClassificationModel(input_path=FLAGS.input_path,
+                                            model_path=FLAGS.model_path,
+                                            config_path=FLAGS.model_config_path,
+                                            tokenizer=tokenizer,
+                                            num_classes=3,
+                                            batch_size=FLAGS.batch_size,
+                                            num_workers=FLAGS.num_workers,
+                                            lr=FLAGS.lr,
+                                            weight_decay=FLAGS.weight_decay,
+                                            warm_up=FLAGS.warm_up)
+    elif FLAGS.label_type == 'minor':
+        model = KbAlbertClassificationModel(input_path=FLAGS.input_path,
+                                            model_path=FLAGS.model_path,
+                                            config_path=FLAGS.model_config_path,
+                                            tokenizer=tokenizer,
+                                            num_classes=4,
+                                            batch_size=FLAGS.batch_size,
+                                            num_workers=FLAGS.num_workers,
+                                            lr=FLAGS.lr,
+                                            weight_decay=FLAGS.weight_decay,
+                                            warm_up=FLAGS.warm_up)
+    else:
+        ValueError('Unknown model type')
+
+    seed_everything(42)
+
+    checkpoint_callback = ModelCheckpoint(
+        filepath=FLAGS.save_dir,
+        save_top_k=1,
+        monitor='val_loss',
+        mode='min'
+    )
+
+    early_stop = EarlyStopping(
+        monitor='val_loss',
+        patience=2,
+        strict=False,
+        verbose=False,
+        mode='min'
+    )
+
+    logger = TensorBoardLogger(
+        save_dir=FLAGS.save_dir,
+        name='logs_' + FLAGS.label_type,
+        version=FLAGS.version
+    )
+    lr_logger = LearningRateLogger()
+
+    if FLAGS.cuda_device > 1:
+        trainer = Trainer(deterministic=True,
+                          gpus=FLAGS.cuda_device,
+                          distributed_backend='ddp',
+                          log_gpu_memory=True,
+                          checkpoint_callback=checkpoint_callback,
+                          early_stop_callback=early_stop,
+                          max_epochs=FLAGS.max_epochs,
+                          logger=logger,
+                          callbacks=[lr_logger])
+        logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
+        logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
+    elif FLAGS.cuda_device == 1:
+        trainer = Trainer(deterministic=True,
+                          gpus=FLAGS.cuda_device,
+                          log_gpu_memory=True,
+                          checkpoint_callback=checkpoint_callback,
+                          early_stop_callback=early_stop,
+                          max_epochs=FLAGS.max_epochs,
+                          logger=logger,
+                          callbacks=[lr_logger])
+        logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
+        logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
+    else:
+        trainer = Trainer(deterministic=True,
+                          checkpoint_callback=checkpoint_callback,
+                          early_stop_callback=early_stop,
+                          max_epochs=FLAGS.max_epochs,
+                          logger=logger,
+                          callbacks=[lr_logger])
+        logging.info('No GPU available, using the CPU instead.')
+    trainer.fit(model)
+
+
+if __name__ == '__main__':
+    flags.mark_flags_as_required([
+        'input_path', 'label_type', 'vocab_path', 'model_path', 'model_config_path', 'save_dir', 'version'
+    ])
+    app.run(main)
